5e1abced60b4f368c9d9829ea5095d1efaf5ed2d
Nick Bilsborrow <nickbilsborrow15@gmail.com>
2021-06-25 12:05:27 +0100

    first commit

---
+++ JavaScript/apps.js
@@ -0,0 +1,11 @@
+const playButton = document.getElementById("playButton");
+const howToPLayTab = document.getElementById("howToPLayTab")
+const gameTab = document.getElementById("gameTab")
+playButton.addEventListener("click",() =>{
+
+    howToPLayTab.style.display= "none"
+    gameTab.style.display= "block"
+
+
+
+})
